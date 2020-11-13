1777c740783e24151c264fb22c5980070f4a4359
marobiana <69413030+marobiana@users.noreply.github.com>
2020-11-13 19:51:34 +0900

    Update ex01.py

--- 12_file/ex01/ex01.py
+++ 12_file/ex01/ex01.py
@@ -1,1 +1,10 @@
+# 217p
+# 파일 쓰기
+#open(file_path, mode, encoding)
+f = open('test1.txt', 'a', encoding='utf8')
+for i in range(1, 11): # 1 ~ 10
+    data = "%d번째 줄입니다.\n" % i
+    print(data)
+    f.write(data)
 
+f.close()
