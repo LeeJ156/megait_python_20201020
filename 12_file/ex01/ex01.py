006cf95eafd883a9735ae030fdcc5c244db45965
marobiana <69413030+marobiana@users.noreply.github.com>
2020-11-13 18:38:50 +0900

    Update ex01.py

--- 12_file/ex01/ex01.py
+++ 12_file/ex01/ex01.py
@@ -1,10 +1,1 @@
-# 2번째 파라미터 옵션
-# w: 새로 쓰기
-# a: 기존 내용에 쓰기
-f = open("test2.txt", 'a', encoding='utf8')
-for i in range(1, 11):
-    data = "%d번째 줄입니다.\n" % i
-    print(data)
-    f.write(data)
 
-f.close()
